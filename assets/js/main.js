d036c36d69c18a9ea4f03db640172dd8de059eb9
Maksim Nizovyi <nizovoy998@gmail.com>
2025-01-13 18:21:52 +0200

    footer & home slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// HEADER
 // Burger menu
 const burgerMenu = document.querySelector(".header__menu-row");
 const burgerBtn = document.querySelector(".header__burger-icon");
@@ -24,3 +25,18 @@ if (burgerBtnClose && overlay) {
     burgerBtnClose.addEventListener("click", closeMenu);
     overlay.addEventListener("click", closeMenu);
 }
+
+// HOME SLIDER
+jQuery(document).ready(function ($) {
+    $('.home-slider__slides').slick({
+      dots: true, // Включаем точки навигации
+      arrows: true, // Стрелки
+      infinite: true, // Бесконечный скролл
+      speed: 500, // Скорость переключения
+      fade: false, // Плавный переход
+      cssEase: 'ease-in-out', // Анимация переключения
+      autoplay: true, // Автопрокрутка
+      autoplaySpeed: 3000 // Задержка между слайдами
+    });
+  });
+  
